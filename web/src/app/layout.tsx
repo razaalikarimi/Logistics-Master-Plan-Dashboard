b5a9b92675bae6a9c113c8b3aec2a91d852fec2b
razaalikarimi <aliraza85211@gmail.com>
2026-05-19 14:21:58 +0530

    style: add global fade-in keyframes and suppress layout hydration warnings

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -26,8 +26,9 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning={true}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning={true}>{children}</body>
     </html>
   );
 }
